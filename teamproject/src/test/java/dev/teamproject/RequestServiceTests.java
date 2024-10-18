9a51136ac97ec130fa1eaf62a4965cfc9941bb63
Yuxuan <yche2656@uni.sydney.edu.au>
2024-10-18 16:11:00 -0400

    fix checkstyle for tests

--- teamproject/src/test/java/dev/teamproject/RequestServiceTests.java
+++ teamproject/src/test/java/dev/teamproject/RequestServiceTests.java
@@ -1,5 +1,9 @@
 package dev.teamproject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
 import dev.teamproject.common.CommonTypes;
 import dev.teamproject.request.Request;
 import dev.teamproject.request.RequestId;
@@ -9,131 +13,129 @@ import dev.teamproject.timeslot.TimeSlot;
 import dev.teamproject.timeslot.TimeSlotService;
 import dev.teamproject.user.User;
 import dev.teamproject.user.UserService;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
-
 class RequestServiceTests {
 
-    @InjectMocks
-    private RequestService requestService;
+  @InjectMocks
+  private RequestService requestService;
 
-    @Mock
-    private RequestRepo requestRepo;
+  @Mock
+  private RequestRepo requestRepo;
 
-    @Mock
-    private UserService userService;
+  @Mock
+  private UserService userService;
 
-    @Mock
-    private TimeSlotService timeSlotService;
+  @Mock
+  private TimeSlotService timeSlotService;
 
-    private Request request;
-    private User user;
-    private TimeSlot timeSlot;
-    private RequestId requestId;
+  private Request request;
+  private User user;
+  private TimeSlot timeSlot;
+  private RequestId requestId;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        user = new User();
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    user = new User();
 
-        timeSlot = new TimeSlot();
-        timeSlot.setTid(1);
+    timeSlot = new TimeSlot();
+    timeSlot.setTid(1);
 
-        request = new Request();
-        request.setUser(user);
-        request.setTimeSlot(timeSlot);
+    request = new Request();
+    request.setUser(user);
+    request.setTimeSlot(timeSlot);
 
-        requestId = new RequestId(timeSlot, user);
-    }
+    requestId = new RequestId(timeSlot, user);
+  }
 
-    @Test
-    void testCreateRequest() {
-        when(userService.findById(user.getUid())).thenReturn(user);
-        when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
-        when(requestRepo.save(request)).thenReturn(request);
+  @Test
+  void testCreateRequest() {
+    when(userService.findById(user.getUid())).thenReturn(user);
+    when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
+    when(requestRepo.save(request)).thenReturn(request);
 
-        Request createdRequest = requestService.createRequest(request);
-        assertEquals(request, createdRequest);
-    }
+    Request createdRequest = requestService.createRequest(request);
+    assertEquals(request, createdRequest);
+  }
 
-    @Test
-    void testDeleteRequest() {
-        when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
-        when(userService.findById(user.getUid())).thenReturn(user);
-        when(requestRepo.findById(requestId)).thenReturn(Optional.of(request));
+  @Test
+  void testDeleteRequest() {
+    when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
+    when(userService.findById(user.getUid())).thenReturn(user);
+    when(requestRepo.findById(requestId)).thenReturn(Optional.of(request));
 
-        requestService.deleteRequest(timeSlot.getTid(), user.getUid());
-    }
+    requestService.deleteRequest(timeSlot.getTid(), user.getUid());
+  }
 
-    @Test
-    void testDeleteRequestNotFound() {
-        when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
-        when(userService.findById(user.getUid())).thenReturn(user);
-        when(requestRepo.findById(requestId)).thenReturn(Optional.empty());
+  @Test
+  void testDeleteRequestNotFound() {
+    when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
+    when(userService.findById(user.getUid())).thenReturn(user);
+    when(requestRepo.findById(requestId)).thenReturn(Optional.empty());
 
-        assertThrows(RuntimeException.class, () -> requestService.deleteRequest(timeSlot.getTid(), user.getUid()));
-    }
+    assertThrows(RuntimeException.class, () -> requestService
+            .deleteRequest(timeSlot.getTid(), user.getUid()));
+  }
 
-    @Test
-    void testUpdateRequestDescription() {
-        when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
-        when(userService.findById(user.getUid())).thenReturn(user);
-        when(requestRepo.findById(requestId)).thenReturn(Optional.of(request));
-        when(requestRepo.save(request)).thenReturn(request);
+  @Test
+  void testUpdateRequestDescription() {
+    when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
+    when(userService.findById(user.getUid())).thenReturn(user);
+    when(requestRepo.findById(requestId)).thenReturn(Optional.of(request));
+    when(requestRepo.save(request)).thenReturn(request);
 
-        Request updatedRequest = requestService.updateRequestDescription(timeSlot.getTid(), user.getUid(), "New Description");
-        assertEquals("New Description", updatedRequest.getDescription());
-    }
+    Request updatedRequest = requestService
+            .updateRequestDescription(timeSlot.getTid(), user.getUid(), "New Description");
+    assertEquals("New Description", updatedRequest.getDescription());
+  }
 
-    @Test
-    void testUpdateRequestStatus() {
-        when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
-        when(userService.findById(user.getUid())).thenReturn(user);
-        when(requestRepo.findById(requestId)).thenReturn(Optional.of(request));
-        when(requestRepo.save(request)).thenReturn(request);
+  @Test
+  void testUpdateRequestStatus() {
+    when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
+    when(userService.findById(user.getUid())).thenReturn(user);
+    when(requestRepo.findById(requestId)).thenReturn(Optional.of(request));
+    when(requestRepo.save(request)).thenReturn(request);
 
-        Request updatedRequest = requestService.updateRequestStatus(timeSlot.getTid(), user.getUid(), CommonTypes.RequestStatus.approved);
-        assertEquals(CommonTypes.RequestStatus.approved, updatedRequest.getStatus());
-    }
+    Request updatedRequest = requestService.updateRequestStatus(timeSlot.getTid(),
+            user.getUid(), CommonTypes.RequestStatus.approved);
+    assertEquals(CommonTypes.RequestStatus.approved, updatedRequest.getStatus());
+  }
 
-    @Test
-    void testGetRequestById() {
-        when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
-        when(userService.findById(user.getUid())).thenReturn(user);
-        when(requestRepo.findById(requestId)).thenReturn(Optional.of(request));
+  @Test
+  void testGetRequestById() {
+    when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
+    when(userService.findById(user.getUid())).thenReturn(user);
+    when(requestRepo.findById(requestId)).thenReturn(Optional.of(request));
 
-        Request foundRequest = requestService.getRequestById(user.getUid(), timeSlot.getTid());
-        assertEquals(request, foundRequest);
-    }
+    Request foundRequest = requestService.getRequestById(user.getUid(), timeSlot.getTid());
+    assertEquals(request, foundRequest);
+  }
 
-    @Test
-    void testGetRequestsByRequester() {
-        when(userService.findById(user.getUid())).thenReturn(user);
-        List<Request> requests = Arrays.asList(request);
-        when(requestRepo.findByUser(user)).thenReturn(requests);
+  @Test
+  void testGetRequestsByRequester() {
+    when(userService.findById(user.getUid())).thenReturn(user);
+    List<Request> requests = Collections.singletonList(request);
+    when(requestRepo.findByUser(user)).thenReturn(requests);
 
-        List<Request> foundRequests = requestService.getRequestsByRequester(user.getUid());
-        assertEquals(requests, foundRequests);
-    }
+    List<Request> foundRequests = requestService.getRequestsByRequester(user.getUid());
+    assertEquals(requests, foundRequests);
+  }
 
-    @Test
-    void testGetRequestsByTimeSlot() {
-        when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
-        List<Request> requests = Arrays.asList(request);
-        when(requestRepo.findByTimeSlot(timeSlot)).thenReturn(requests);
+  @Test
+  void testGetRequestsByTimeSlot() {
+    when(timeSlotService.getTimeSlotById(timeSlot.getTid())).thenReturn(timeSlot);
+    List<Request> requests = Collections.singletonList(request);
+    when(requestRepo.findByTimeSlot(timeSlot)).thenReturn(requests);
 
-        List<Request> foundRequests = requestService.getRequestsByTimeSlot(timeSlot.getTid());
-        assertEquals(requests, foundRequests);
-    }
+    List<Request> foundRequests = requestService.getRequestsByTimeSlot(timeSlot.getTid());
+    assertEquals(requests, foundRequests);
+  }
 }
--- teamproject/src/test/java/dev/teamproject/TimeSlotControllerTests.java
+++ teamproject/src/test/java/dev/teamproject/TimeSlotControllerTests.java
@@ -1,9 +1,14 @@
 package dev.teamproject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 import dev.teamproject.common.CommonTypes;
 import dev.teamproject.timeslot.TimeSlot;
 import dev.teamproject.timeslot.TimeSlotController;
 import dev.teamproject.timeslot.TimeSlotService;
+import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -11,98 +16,93 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-
 class TimeSlotControllerTests {
 
-    @InjectMocks
-    private TimeSlotController timeSlotController;
+  @InjectMocks
+  private TimeSlotController timeSlotController;
 
-    @Mock
-    private TimeSlotService timeSlotService;
+  @Mock
+  private TimeSlotService timeSlotService;
 
-    private TimeSlot timeSlot;
+  private TimeSlot timeSlot;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        timeSlot = new TimeSlot();
-        timeSlot.setTid(1);
-    }
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    timeSlot = new TimeSlot();
+    timeSlot.setTid(1);
+  }
 
-    @Test
-    void testCreateTimeSlot() {
-        when(timeSlotService.createTimeSlot(timeSlot)).thenReturn(timeSlot);
-        ResponseEntity<TimeSlot> response = timeSlotController.createTimeSlot(timeSlot);
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals(timeSlot, response.getBody());
-    }
+  @Test
+  void testCreateTimeSlot() {
+    when(timeSlotService.createTimeSlot(timeSlot)).thenReturn(timeSlot);
+    ResponseEntity<TimeSlot> response = timeSlotController.createTimeSlot(timeSlot);
+    assertEquals(200, response.getStatusCodeValue());
+    assertEquals(timeSlot, response.getBody());
+  }
 
-    @Test
-    void testGetTimeSlotById() {
-        int tid = 1;
-        when(timeSlotService.getTimeSlotById(tid)).thenReturn(timeSlot);
-        ResponseEntity<TimeSlot> response = timeSlotController.getTimeSlotById(tid);
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals(timeSlot, response.getBody());
-    }
+  @Test
+  void testGetTimeSlotById() {
+    int tid = 1;
+    when(timeSlotService.getTimeSlotById(tid)).thenReturn(timeSlot);
+    ResponseEntity<TimeSlot> response = timeSlotController.getTimeSlotById(tid);
+    assertEquals(200, response.getStatusCodeValue());
+    assertEquals(timeSlot, response.getBody());
+  }
 
-    @Test
-    void testGetAllTimeSlots() {
-        List<TimeSlot> timeSlots = Arrays.asList(timeSlot);
-        when(timeSlotService.getAllTimeSlots()).thenReturn(timeSlots);
-        ResponseEntity<List<TimeSlot>> response = timeSlotController.getAllTimeSlots();
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals(timeSlots, response.getBody());
-    }
+  @Test
+  void testGetAllTimeSlots() {
+    List<TimeSlot> timeSlots = Collections.singletonList(timeSlot);
+    when(timeSlotService.getAllTimeSlots()).thenReturn(timeSlots);
+    ResponseEntity<List<TimeSlot>> response = timeSlotController.getAllTimeSlots();
+    assertEquals(200, response.getStatusCodeValue());
+    assertEquals(timeSlots, response.getBody());
+  }
 
-    @Test
-    void testGetTimeSlotsByUser() {
-        int uid = 1;
-        List<TimeSlot> timeSlots = Arrays.asList(timeSlot);
-        when(timeSlotService.getTimeSlotsByUser(uid)).thenReturn(timeSlots);
-        ResponseEntity<List<TimeSlot>> response = timeSlotController.getTimeSlotsByUser(uid);
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals(timeSlots, response.getBody());
-    }
+  @Test
+  void testGetTimeSlotsByUser() {
+    int uid = 1;
+    List<TimeSlot> timeSlots = Collections.singletonList(timeSlot);
+    when(timeSlotService.getTimeSlotsByUser(uid)).thenReturn(timeSlots);
+    ResponseEntity<List<TimeSlot>> response = timeSlotController.getTimeSlotsByUser(uid);
+    assertEquals(200, response.getStatusCodeValue());
+    assertEquals(timeSlots, response.getBody());
+  }
 
-    @Test
-    void testGetTimeSlotsByDay() {
-        CommonTypes.Day day = CommonTypes.Day.Monday;
-        List<TimeSlot> timeSlots = Arrays.asList(timeSlot);
-        when(timeSlotService.getTimeSlotsByDay(day)).thenReturn(timeSlots);
-        ResponseEntity<List<TimeSlot>> response = timeSlotController.getTimeSlotsByDay(day);
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals(timeSlots, response.getBody());
-    }
+  @Test
+  void testGetTimeSlotsByDay() {
+    CommonTypes.Day day = CommonTypes.Day.Monday;
+    List<TimeSlot> timeSlots = Collections.singletonList(timeSlot);
+    when(timeSlotService.getTimeSlotsByDay(day)).thenReturn(timeSlots);
+    ResponseEntity<List<TimeSlot>> response = timeSlotController.getTimeSlotsByDay(day);
+    assertEquals(200, response.getStatusCodeValue());
+    assertEquals(timeSlots, response.getBody());
+  }
 
-    @Test
-    void testGetTimeSlotsByAvailability() {
-        CommonTypes.Availability availability = CommonTypes.Availability.available;
-        List<TimeSlot> timeSlots = Arrays.asList(timeSlot);
-        when(timeSlotService.getTimeSlotsByAvailability(availability)).thenReturn(timeSlots);
-        ResponseEntity<List<TimeSlot>> response = timeSlotController.getTimeSlotsByAvailability(availability);
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals(timeSlots, response.getBody());
-    }
+  @Test
+  void testGetTimeSlotsByAvailability() {
+    CommonTypes.Availability availability = CommonTypes.Availability.available;
+    List<TimeSlot> timeSlots = Collections.singletonList(timeSlot);
+    when(timeSlotService.getTimeSlotsByAvailability(availability)).thenReturn(timeSlots);
+    ResponseEntity<List<TimeSlot>> response = timeSlotController
+            .getTimeSlotsByAvailability(availability);
+    assertEquals(200, response.getStatusCodeValue());
+    assertEquals(timeSlots, response.getBody());
+  }
 
-    @Test
-    void testUpdateTimeSlot() {
-        int tid = 1;
-        when(timeSlotService.updateTimeSlot(tid, timeSlot)).thenReturn(timeSlot);
-        ResponseEntity<TimeSlot> response = timeSlotController.updateTimeSlot(tid, timeSlot);
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals(timeSlot, response.getBody());
-    }
+  @Test
+  void testUpdateTimeSlot() {
+    int tid = 1;
+    when(timeSlotService.updateTimeSlot(tid, timeSlot)).thenReturn(timeSlot);
+    ResponseEntity<TimeSlot> response = timeSlotController.updateTimeSlot(tid, timeSlot);
+    assertEquals(200, response.getStatusCodeValue());
+    assertEquals(timeSlot, response.getBody());
+  }
 
-    @Test
-    void testDeleteTimeSlot() {
-        int tid = 1;
-        ResponseEntity<Void> response = timeSlotController.deleteTimeSlot(tid);
-        assertEquals(204, response.getStatusCodeValue());
-    }
+  @Test
+  void testDeleteTimeSlot() {
+    int tid = 1;
+    ResponseEntity<Void> response = timeSlotController.deleteTimeSlot(tid);
+    assertEquals(204, response.getStatusCodeValue());
+  }
 }
